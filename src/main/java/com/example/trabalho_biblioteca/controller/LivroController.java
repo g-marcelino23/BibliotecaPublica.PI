a5d8a94d7add5664e1dbc03d1e2073117990a175
Lucas Rama <lucasrama2002@gmail.com>
2025-04-25 03:29:08 -0300

    Adicionando CrossOrigin ao Controller de Livro

--- src/main/java/com/example/trabalho_biblioteca/controller/LivroController.java
+++ src/main/java/com/example/trabalho_biblioteca/controller/LivroController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/livro")
+@CrossOrigin(origins = "*")
 public class LivroController {
     @Autowired
     LivroService livroService;
